b1710ce07f2494fbfd57c4b9d3bc2115b249898d
Alsanad-Opium <alsanad112006@gmail.com>
2025-01-30 23:39:51 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const colorPicker = document.getElementById('colorPicker')
+
+const bgcolor = document.getElementById('bgcolor')
+
+const fsize = document.getElementById('fontPicker')
+
+const canvas = document.getElementById('canvas')
+
+const clearBtn = document.getElementById('clear')
+
+const saveCanvas = document.getElementById('SaveCanvas')
+
+const retreive = document.getElementById('retreive')
+
+const ctx = document.getContent('canvas')
+
