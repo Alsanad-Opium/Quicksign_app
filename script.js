c924b8492f2a6fa1de378b4c510277c01cf4b979
Alsanad-Opium <alsanad112006@gmail.com>
2025-01-31 00:53:10 +0530

    add the draw feature in this

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const colorPicker = document.getElementById('colorPicker')
 
-const bgcolor = document.getElementById('bgcolor')
+const bgcolor = document.getElementById('bgColor')
 
 const fsize = document.getElementById('fontPicker')
 
@@ -12,5 +12,47 @@ const saveCanvas = document.getElementById('SaveCanvas')
 
 const retreive = document.getElementById('retreive')
 
-const ctx = document.getContent('canvas')
+const ctx = canvas.getContext('2d')
 
+let isDrawing
+
+colorPicker.addEventListener('change',(e)=>{
+    ctx.strokeStyle = e.target.value
+
+    ctx.fillStyle = e.target.value
+})
+
+canvas.addEventListener('mousedown',(e)=>{
+
+    isDrawing = true
+     lastX = e.offsetX
+     lastY = e.offsetY
+})
+
+canvas.addEventListener('mousemove', (e)=>{
+
+    if (isDrawing) {
+        ctx.beginPath()
+        ctx.moveTo(lastX,lastY)
+        ctx.lineTo(e.offsetX, e.offsetY)
+        ctx.stroke()
+
+        lastX = e.offsetX
+        lastY = e.offsetY
+    }
+})
+
+
+canvas.addEventListener('mouseup', (e)=>{
+    isDrawing = false
+})
+
+bgcolor.addEventListener('change',(e)=>{
+   
+    ctx.fillStyle = e.target.value
+    ctx.fillRect(0,0, canvas.width, canvas.height)
+})
+
+clearBtn.addEventListener('click', ()=>{
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+})
